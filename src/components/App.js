1667a9727d81a9c0f0ff495e37bfafc34d095b5a
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-02-17 16:05:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { useState } from 'react';
 import '../styles/App.css';
 
 const data = {
@@ -27,10 +27,38 @@ const data = {
   ]
 }
 const App = () => {
+  let [movieList, setMovieList]= useState([]);
+  let [text, setText]=useState("No year selected");
+  const getMovies = (event)=>{
+    const key=event.target.value;
+    setText("Selected year-"+key);
+    for(const element in data){
+      if(element==key){
+        setMovieList(data[element]);
+        return;
+      }
+    };
+    return;
+  }
 
   return (
     <div id="main">
-      
+      <select name="" id="" onChange={getMovies}>
+        <option name="year" value="" > </option>
+        <option name="year" value="2018">2018</option>
+        <option name="year" value="2019">2019</option>
+        <option name="year" value="2020">2020</option>
+        <option name="year" value="2021">2021</option>
+        <option name="year" value="2022">2022</option>
+      </select>
+      <p>{text}</p>
+      <div>
+        <ul>
+          {movieList.map((ele, index)=>{
+            return <li key={index}>{ele}</li>
+          })}
+        </ul>
+      </div>
     </div>
   )
 }
